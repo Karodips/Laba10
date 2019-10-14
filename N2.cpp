0340503d0fb5f806a1e2aeb4d4bc92f8d7bda105
Karodips <54810893+Karodips@users.noreply.github.com>
2019-10-14 12:43:33 +0300

    Create N2.cpp

---
+++ N2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	LC_ALL(0, "");
+	int n;
+	double i,res;
+	res = 1;
+	cin >> n;
+	for (i = 1.1; i <= n; i += 0.1) res = res * i;
+	res = res * n;
+	cout << res << endl;
+	system("pause");
+	return(0);
+}
