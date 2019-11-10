704412182b301cbbc97c974aa41613ce1ffaf055
Karodips <54810893+Karodips@users.noreply.github.com>
2019-11-10 18:41:51 +0300

    Update N2.cpp

--- N2.cpp
+++ N2.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
 	LC_ALL(0, "");
-	int n;
-	double i,res;
+	double i, res,n;
 	res = 1;
 	cin >> n;
+	n = n / 10 + 1;
 	for (i = 1.1; i <= n; i += 0.1) res = res * i;
 	res = res * n;
 	cout << res << endl;
